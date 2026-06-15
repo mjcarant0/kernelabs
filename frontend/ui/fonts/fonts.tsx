eda847d75ad6025be7a1d266939c09aff66b4c3b
John Reydo Tinawin <johnreydo.tinawin@gmail.com>
2026-06-15 12:51:27 +0800

    feat: add Open Sans font setup

---
+++ frontend/ui/fonts/fonts.tsx
@@ -0,0 +1,9 @@
+import { Open_Sans } from "next/font/google";
+
+export const openSans = Open_Sans({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700", "800"],
+  style: ["normal", "italic"],
+  variable: "--font-open-sans",
+  display: "swap",
+});
